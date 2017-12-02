aa456fe9d20709ec94bf9791778275bcadada23d
Jacks0N23 <asgu23@gmail.com>
2017-12-02 20:01:18 +0300

    fix

--- main.py
+++ main.py
@@ -67,14 +67,19 @@ def parse_request(date, intent_name, uid):
 
 
 def build_message_and_send():
-    is_free = "Посещение бесплатное" if parsed_list[last_post_position]['isFree'] == "true" else "Посещение платное"
+    try:
+        is_free = "Посещение бесплатное" if parsed_list[last_post_position]['isFree'] == "true" else "Посещение платное"
 
-    price = str(parsed_list[last_post_position]['price']) + "руб." if is_free == "Посещение платное" else "\n"
+        price = str(parsed_list[last_post_position]['price']) + "руб." if is_free == "Посещение платное" else "\n"
 
-    user_message = "*" + parsed_list[last_post_position]['name'] + "*" + "\n" + parsed_list[last_post_position][
-        'shortDescription'] + "\n" + is_free + " " + price
+        user_message = "*" + parsed_list[last_post_position]['name'] + "*" + "\n" + parsed_list[last_post_position][
+            'shortDescription'] + "\n" + is_free + " " + price
 
-    bot.send_message(user_id, user_message, parse_mode="Markdown", reply_markup=utils.generate_markup_keyboard(["Ещё"]))
+        bot.send_message(user_id, user_message, parse_mode="Markdown",
+                         reply_markup=utils.generate_markup_keyboard(["Ещё"]))
+
+    except IndexError:
+        bot.send_message(user_id, "Больше нет(")
 
 
 def process_command(response):
