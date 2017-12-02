aad9de7d45ec3764a66d3a9af44f4852593971ec
Jacks0N23 <asgu23@gmail.com>
2017-12-02 13:46:10 +0300

    setup

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
 
 import telebot
+from flask import Flask
 
 import config
 
 bot = telebot.TeleBot(config.token)
-bot.stop_polling()
+
+HOST = "0.0.0.0"
+PORT = os.environ.get('PORT', 8443)
+
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=["start"])
@@ -25,4 +31,25 @@ def process_command(message):
         bot.send_message(message.from_user.id, message.text)
 
 
-bot.polling(none_stop=True)
+@server.route('/bot', methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return '/bot', 200
+
+
+@server.route('/')
+def webhook_handler():
+    bot.remove_webhook()
+    bot.set_webhook(url=config.heroku_webhook)
+    status_msg = "i'm live. listening on %s:%s" % (HOST, PORT)
+    return status_msg, 200
+
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=config.heroku_webhook)
+
+# bot.polling(none_stop=True)
+server.run(host=HOST, port=PORT)
